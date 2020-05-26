b389ad1c1f2d7a09e873be75b6c385edb1cd3abf
kevin <kf@vestjyskmarketing.dk>
2020-05-26 11:00:45 +0200

    Error correction in tag endings

--- functions.py
+++ functions.py
@@ -10,9 +10,9 @@ def get_domain_file_text(domain):
     domainFileText += '\n\t<content url="file://L:/' + domain + '">'
     domainFileText += '\n\t\t<excludeFolder url="file://L:/' + domain + '/www/app/webroot/_resized" />'
     domainFileText += '\n\t\t<excludeFolder url="file://L:/' + domain + '/www/app/webroot/uploads" />'
-    domainFileText += '\n\t\t<excludeFolder url="file://L:/' + domain + '/www/backup"> /'
+    domainFileText += '\n\t\t<excludeFolder url="file://L:/' + domain + '/www/backup" />'
     domainFileText += """\n\t</content>
-        orderEntry type="inheritedJdk" />
+        <orderEntry type="inheritedJdk" />
         <orderEntry type="sourceFolder" forTests="false" />
       </component>
     </module>"""
